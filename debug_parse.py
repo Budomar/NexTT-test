84420d2741c6dfddab3000b60558f1cc6012b290
Abdullin Oleg <wimbd@yandex.ru>
2026-07-01 11:29:15 +0300

    Повторно Исправлен парсинг комплектующих, добавлен поиск фланцев

--- debug_parse.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from nextt.core.normalizer import SpecNormalizer
-
-if __name__ == "__main__":
-    n = SpecNormalizer(debug=True)
-    n.debug_parse("VK-Profil 22-03-12 la")
